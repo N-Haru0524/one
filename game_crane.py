cdb540b0b138e7d8560b8a498ab4c84c4c4d7074
N-Haru0524 <u598104j@ecs.osaka-u.ac.jp>
2026-01-14 22:05:48 +0900

    feat: update gripper initialization and fix robot state reference in catch_target function

--- game_crane.py
+++ game_crane.py
@@ -6,7 +6,7 @@ robot = khi_rs007l.RS007L()
 robot.attach_to(base.scene)
 robot.toggle_render_collision=True
 
-gripper = or_2fg7.OR2FG7(base_pos=(0.3, 0, 0.5))
+gripper = or_2fg7.OR2FG7()
 gripper.attach_to(base.scene)
 
 target_box = ossop.gen_box(spos=(.3, 0, .5), size=(.05, .05, .05))
@@ -15,7 +15,7 @@ target_box.attach_to(base.scene)
 robot.engage(gripper)
 
 def catch_target(dt):
-    tgt_homo = robot.fk(robot.state.qs, update=False)[-1]
+    tgt_homo = robot.fk(robot.qs, update=False)[-1]
     tgt_pos = tgt_homo[:3, 3]
     tgt_rotmat = tgt_homo[:3, :3]
     print("TCP homo:", tgt_homo)
